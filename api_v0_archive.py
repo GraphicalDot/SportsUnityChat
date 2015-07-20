f470cb0e60f4b20ae61fa9b1eac1020f296552b3
Satish <satishck1992@gmail.com>
2015-07-20 15:33:09 +0530

    Added pretty logging

--- api_v0_archive.py
+++ api_v0_archive.py
@@ -1,6 +1,7 @@
 import tornado.ioloop
 import tornado.web
 import psycopg2
+from tornado.log import enable_pretty_logging
 
 class QueryHandler:
 	@classmethod
@@ -13,12 +14,14 @@ class ArchiveAcessHandler(tornado.web.RequestHandler):
     def get(self):
     	from_timestamp = str(self.get_arguments("from", True)) 
         to_timestamp = str(self.get_arguments("to", True))
+        print(from_timestamp)
+        print(to_timestamp)
         skip = self.get_arguments("skip", True) or 0
     	limit = self.get_arguments("limit", True) or 100
         response = {}
         response['version'] = 0.1
         try:
-        	response['info'] = QueryHandler.get_results(" SELECT txt FROM archive WHERE timestamp > (%s) AND timestamp < (%s) OFFSET (%s) LIMIT (%s); ",(from_timestamp, to_timestamp, skip, limit, ))
+        	response['info'] = QueryHandler.get_results(" SELECT txt FROM archive WHERE timestamp > (%s) AND timestamp < (%s) OFFSET (%s) LIMIT (%s); ",(from_timestamp, to_timestamp, skip, limit,))
         	response['status'] = 200
         except Exception, e:
             response['info'] = " Error: %s" % e
@@ -28,8 +31,10 @@ class ArchiveAcessHandler(tornado.web.RequestHandler):
 application = tornado.web.Application([
     (r"/messages", ArchiveAcessHandler),
 ], 
-autoreload = True)
+autoreload = True,
+)
 
 if __name__ == "__main__":
+    enable_pretty_logging()
     application.listen(3000)
     tornado.ioloop.IOLoop.current().start()
