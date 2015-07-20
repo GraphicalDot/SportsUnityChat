b4fa7cc3bc4e06ed28d7f0ad95a4739eee89d9c3
Satish <satishck1992@gmail.com>
2015-07-20 15:22:35 +0530

    finalising api

--- api_v0_archive.py
+++ api_v0_archive.py
@@ -11,14 +11,14 @@ class QueryHandler:
 
 class ArchiveAcessHandler(tornado.web.RequestHandler):
     def get(self):
-    	from_timestamp = self.get_arguments("from", True) 
-        to_timestamp = self.get_arguments("to", True)
+    	from_timestamp = str(self.get_arguments("from", True)) 
+        to_timestamp = str(self.get_arguments("to", True))
         skip = self.get_arguments("skip", True) or 0
     	limit = self.get_arguments("limit", True) or 100
         response = {}
         response['version'] = 0.1
         try:
-        	response['info'] = QueryHandler.get_results(" SELECT txt FROM archives WHERE timestamp > (%s) AND timestamp < (%s) OFFSET (%s) LIMIT (%s); ",(from_timestamp, to_timestamp, skip, limit, ))
+        	response['info'] = QueryHandler.get_results(" SELECT txt FROM archive WHERE timestamp > (%s) AND timestamp < (%s) OFFSET (%s) LIMIT (%s); ",(from_timestamp, to_timestamp, skip, limit, ))
         	response['status'] = 200
         except Exception, e:
             response['info'] = " Error: %s" % e
