2759d19c0e10fca8e30af62b3929b7d2702194c1
Satish <satishck1992@gmail.com>
2015-07-20 14:37:01 +0530

    First api commit

--- api_v0_archive.py
+++ api_v0_archive.py
@@ -7,25 +7,26 @@ class QueryHandler:
 	def get_results(cls, query, variables):
 		connection = psycopg2.connect("dbname=development host=localhost user=development password=development")
 		cursor = connection.cursor()
-		return cursor.execute("SELECT * FROM logs;")
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world ")
+		return cursor.execute(query, variables)
 
 class ArchiveAcessHandler(tornado.web.RequestHandler):
     def get(self):
-    	from_timestamp = self.get_arguments("from_timestamp", True) 
-    	to_timestamp = self.get_arguments("to_timestamp", True)
-    	example = {}
-    	example['info'] = QueryHandler.get_results("","")
-    	self.write(example)
+    	from_timestamp = self.get_arguments("from", True) 
+        to_timestamp = self.get_arguments("to", True)
+        skip = self.get_arguments("skip", True) or 0
+    	limit = self.get_arguments("limit", True) or 100
+        response = {}
+        response['version'] = 0.1
+        try:
+        	response['info'] = QueryHandler.get_results(" SELECT txt FROM archives WHERE timestamp > (%s) AND timestamp < (%s) OFFSET (%s) LIMIT (%s); ",(from_timestamp, to_timestamp, skip, limit, ))
+        	response['status'] = 200
+        except Exception, e:
+            response['info'] = " An error has occured. Developers should be notified "
+            response['status'] = 500
+        self.write(response)
 
 application = tornado.web.Application([
     (r"/messages", ArchiveAcessHandler),
-    (r"/", MainHandler),
-    (r"/", MainHandler),
-    (r"/", MainHandler),
 ], 
 autoreload = True)
 
