9a6c6216141f27f1aaa060c91691a04c8fbea303
kaali <satishck1992@gmail.com>
2016-03-22 12:56:26 +0530

    Added config

---
+++ config.py
@@ -0,0 +1,36 @@
+[database]
+database=test_database
+host=masterdb
+user=aakarshi
+password=aakarshi
+
+[registration]
+expiry_period_sec=1800
+
+[pubsub]
+pubsub_addr=pubsub.mm.io
+pubsub_jid=satish@mm.io
+pubsub_password=password
+sample_message=Lorem Ipsum is simply dummy text of the printing and typesetting industry please consider donating a small sum to help pay for the hosting and bandwidth bill
+node=princely_musings
+
+[xmpp]
+domain=@mm.io
+
+[tests]
+test_facebook_id=145634995501895
+test_phone_number=919560488236
+profile_pic_url = http://localhost:3000/profile_pic
+
+[tornado]
+listening_port=3000
+
+[nearby_users]
+was_online_limit=1800
+
+[apns]
+cert_file='cert.pem'
+key_file='key.pem'
+
+[gcm]
+api_key=''
