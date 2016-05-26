d84286f1d586431813b9d24ad52aa2761525dfca
madmachines <satishck1992@gmail.com>
2016-05-26 15:24:03 +0530

    Resolved dependency bug in migrations

--- migrations/20160525_03_6NW5O-add-coupons-tables.py
+++ migrations/20160525_03_6NW5O-add-coupons-tables.py
@@ -4,7 +4,7 @@ Add coupons tables
 
 from yoyo import step
 
-__depends__ = {'20160525_02_wURid-add-table-for-coupons'}
+__depends__ = {}
 
 steps = [
     step(" CREATE TABLE coupons (code TEXT PRIMARY KEY, created_at TIMESTAMP DEFAULT now(), coupon_limit integer NOT NULL, used_count integer NOT NULL DEFAULT 0 ) ")
