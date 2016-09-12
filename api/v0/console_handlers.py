7c02604debbd36172207e19daa5a4121c4a738ce
satishck1992 <satishck1992@gmail.com>
2016-09-12 13:25:53 +0530

    Update console_handlers.py

--- api/v0/console_handlers.py
+++ api/v0/console_handlers.py
@@ -433,7 +433,7 @@ class PeekDiscussionsHandler(BaseRequestHandler):
         response = {}
         discussion_id = self.get_argument("discussion_id")
         username = self.get_argument("username")
-        Discussion(discussion_id).subscribe_user(username)
+        Discussion(discussion_id).subscribe_user(info)
         response['info'] = settings.SUCCESS_RESPONSE
         response['status'] = settings.STATUS_200        
         self.write(response)
