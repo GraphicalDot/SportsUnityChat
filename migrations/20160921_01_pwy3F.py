ae064c399bbaa3298302871d8cc2c25f74752947
madmachines <satishck1992@gmail.com>
2016-09-21 10:47:01 +0530

    Added migrations for default values of tables

---
+++ migrations/20160921_01_pwy3F.py
@@ -0,0 +1,14 @@
+"""
+Add default values for tables related to users discussions parameters
+"""
+
+from yoyo import step
+
+__depends__ = {'20160808_02_j3Wv3-add-tables-for-limiting-parameters-in-case-of-chat-api'}
+
+steps = [
+    step(" WITH upsert AS (UPDATE min_discussion_users SET count = 2 RETURNING * ) INSERT INTO min_discussion_users (count) SELECT  2 WHERE NOT EXISTS (SELECT * FROM upsert)"),
+    step(" WITH upsert AS (UPDATE max_discussion_users SET count = 8 RETURNING * ) INSERT INTO max_discussion_users (count) SELECT  8 WHERE NOT EXISTS (SELECT * FROM upsert)"),
+    step(" WITH upsert AS (UPDATE max_discussion_ratio SET count = 10 RETURNING * ) INSERT INTO max_discussion_ratio (count) SELECT  10 WHERE NOT EXISTS (SELECT * FROM upsert)"),
+    step(" WITH upsert AS (UPDATE min_discussion_users_ratio SET count = 0.01 RETURNING * ) INSERT INTO min_discussion_users_ratio (count) SELECT  0.01 WHERE NOT EXISTS (SELECT * FROM upsert)")
+]
